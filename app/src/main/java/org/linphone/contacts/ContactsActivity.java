4c6dc85ff3065d493b9580bef5565c197335fcb5
Stefanos Togkoulidis <stefanostogoulidis@gmail.com>
2021-01-18 04:15:19 +0200

    Full screen immersive mode

--- app/src/main/java/org/linphone/contacts/ContactsActivity.java
+++ app/src/main/java/org/linphone/contacts/ContactsActivity.java
@@ -24,6 +24,7 @@ import android.app.Fragment;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Toast;
 import org.linphone.R;
 import org.linphone.activities.MainActivity;
@@ -98,6 +99,14 @@ public class ContactsActivity extends MainActivity {
     @Override
     protected void onResume() {
         super.onResume();
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN);
     }
 
     @Override
