094f8bc3b4630ea9c5db63aa2a2cb3daa3bd1d0a
Stefanos Togkoulidis <stefanostogoulidis@gmail.com>
2021-01-20 00:05:27 +0200

    Return to contacts list after adding a new contact

--- app/src/main/java/org/linphone/contacts/ContactEditorFragment.java
+++ app/src/main/java/org/linphone/contacts/ContactEditorFragment.java
@@ -202,9 +202,6 @@ public class ContactEditorFragment extends Fragment {
                         }
 
                         getFragmentManager().popBackStack();
-                        if (mIsNewContact || getResources().getBoolean(R.bool.isTablet)) {
-                            ((ContactsActivity) getActivity()).showContactDetails(mContact);
-                        }
                     }
                 });
 
@@ -286,7 +283,7 @@ public class ContactEditorFragment extends Fragment {
                                         @Override
                                         public void onClick(View view) {
                                             mContact.delete();
-                                            ((ContactsActivity) getActivity()).goBack();
+                                            getFragmentManager().popBackStack();
                                             dialog.dismiss();
                                         }
                                     });
