e1bbcfa07b51bb438ba081edec319130beeecf2a
Stefanos Togkoulidis <stefanostogoulidis@gmail.com>
2021-01-27 01:46:25 +0200

    Make all LinphoneGenericActivity activities fullscreen

--- app/src/main/java/org/linphone/activities/LinphoneGenericActivity.java
+++ app/src/main/java/org/linphone/activities/LinphoneGenericActivity.java
@@ -22,6 +22,7 @@ package org.linphone.activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Surface;
+import android.view.View;
 import org.linphone.LinphoneContext;
 import org.linphone.LinphoneManager;
 import org.linphone.core.Core;
@@ -39,6 +40,8 @@ public abstract class LinphoneGenericActivity extends ThemeableActivity {
     @Override
     protected void onResume() {
         super.onResume();
+
+        makeFullscreen();
 
         ensureServiceIsRunning();
 
@@ -73,6 +76,17 @@ public abstract class LinphoneGenericActivity extends ThemeableActivity {
                 core.setDeviceRotation(rotation);
             }
         }
+    }
+
+    public void makeFullscreen() {
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN);
     }
 
     private void ensureServiceIsRunning() {
--- app/src/main/java/org/linphone/activities/MainActivity.java
+++ app/src/main/java/org/linphone/activities/MainActivity.java
@@ -658,15 +658,4 @@ public abstract class MainActivity extends LinphoneGenericActivity
     public SideMenuFragment getSideMenuFragment() {
         return mSideMenuFragment;
     }
-
-    public void makeFullscreen() {
-        View decorView = getWindow().getDecorView();
-        decorView.setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_FULLSCREEN);
-    }
 }
